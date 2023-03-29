b514e465a0f10411edbf2d6fefc4d630048353dc
Tene Boothe <teneaboothe@gmail.com>
2023-03-28 21:08:21 -0400

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -14,6 +16,11 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />}/>
         <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route path='about' element={<AboutPage />} />
+        <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
       </Routes>
       <Footer />
     </div>
